d8f3da8e5317601e012213f17c0b1c2b71831211
Gopal <gselvaraj@verisk.com>
2021-10-07 21:46:43 -0400

    modification

--- src/main/java/com/gs/dictionary/AllienDictionary.java
+++ src/main/java/com/gs/dictionary/AllienDictionary.java
@@ -1,45 +1,55 @@
 package com.gs.dictionary;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class AllienDictionary {
 
-	private Map<Character, Integer> indexMap = new HashMap<Character, Integer>();
+  public boolean isAlienSorted(String[] words, String order) {
+    if (words == null || words.length == 0) {
+      return true;
+    }
+    Map<Character, Integer> charPostionMap = buildCharPositionMap(order);
+    for (int i = 1; i < words.length; i++) {
+      boolean isOrder = isOrder(words[i - 1], words[i], charPostionMap);
+      if (!isOrder) {
+        return false;
+      }
+    }
+    return true;
+  }
 
-	public boolean isAlienSorted(String[] words, String order) {
-		buildIndexMap(order);
-		for (int index = 1; index < words.length; index++) {
-			String prev = words[index - 1];
-			String current = words[index];
-			if ( !isOrder(prev, current)) {
-				return false;
-			}
-		}
-		return true;
-	}
+  private boolean isOrder(String prev, String current, Map<Character, Integer> charPostionMap) {
+    if (prev == null) {
+      return true;
+    }
+    if (current == null || (prev.length() > current.length()) ) { 
+      return false;
+    }
+    return isOrderHelper(prev, current, charPostionMap);
+  }
 
-	private boolean isOrder(String prev, String current) {
-		if (prev == null) return true;
-		
-		if (current == null || prev.length() > current.length()) return false;
-		
-		int limit = Math.min(prev.length(), current.length());
-		for (int index = 0; index < limit; index++) {
-			int prevIndexPos = indexMap.get(prev.charAt(index));
-			int currentIndexPos = indexMap.get(current.charAt(index));
-			if (prevIndexPos > currentIndexPos) {
-				return false;
-			}
-		}
-		return true;
-	}
+  private boolean isOrderHelper(String prev, String current,
+      Map<Character, Integer> charPostionMap) {
+    int limit = Math.min(prev.length(), current.length());
+    for (int i = 0; i < limit; i++) {
+      char p = prev.charAt(i);
+      char c = current.charAt(i);
+      if (p != c) {
+        return charPostionMap.get(p) < charPostionMap.get(c);
+      }
+    }
+    return true;
+  }
 
-	private void buildIndexMap(String order) {
-		int count = 0;
-		for (char c : order.toCharArray()) {
-			indexMap.put(c, count++);
-		}
-	}
+  private Map<Character, Integer> buildCharPositionMap(String order) {
+    Map<Character, Integer> charPostionMap = new HashMap<>();
+    int index = 0;
+    for (char ch : order.toCharArray()) {
+      charPostionMap.put(ch, index);
+      index++;
+    }
+    return charPostionMap;
+  }
+
 
 }
--- src/test/java/com/gs/dictionary/AllienDictionaryTests.java
+++ src/test/java/com/gs/dictionary/AllienDictionaryTests.java
@@ -2,42 +2,41 @@ package com.gs.dictionary;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
 public class AllienDictionaryTests {
 
-	private AllienDictionary allienDictionary = new AllienDictionary();
-	private final String order = "hlabcdefgijkmnopqrstuvwxyz";
-	
-	@Test
-	public void testWithNull() {
-		String[] words = { null, "hello", "leetoode" };
-		assertTrue(allienDictionary.isAlienSorted(words, order));
-	}
-	
-	@Test
-	public void testWithNullCase2() {
-		String[] words = {  "hello", null, "leetoode" };
-		assertFalse(allienDictionary.isAlienSorted(words, order));
-	}
-	
-	@Test
-	public void testCase1() {
-		String[] words = { "apple", "app" };
-		assertFalse(allienDictionary.isAlienSorted(words, order));
-	}
-	
-	@Test
-	public void testCase2() {
-		String[] words = { "app", "apple" };
-		assertTrue(allienDictionary.isAlienSorted(words, order));
-	}
-	
-	@Test
-	public void testCase3() {
-		String[] words = { "app", "", "apple" };
-		assertFalse(allienDictionary.isAlienSorted(words, order));
-	}
-	
+  private AllienDictionary allienDictionary = new AllienDictionary();
+  private final String order = "hlabcdefgijkmnopqrstuvwxyz";
+
+  @Test
+  public void testWithNull() {
+    String[] words = {null, "hello", "leetoode"};
+    assertTrue(allienDictionary.isAlienSorted(words, order));
+  }
+
+  @Test
+  public void testWithNullCase2() {
+    String[] words = {"hello", null, "leetoode"};
+    assertFalse(allienDictionary.isAlienSorted(words, order));
+  }
+
+  @Test
+  public void testCase1() {
+    String[] words = {"apple", "app"};
+    assertFalse(allienDictionary.isAlienSorted(words, order));
+  }
+
+  @Test
+  public void testCase2() {
+    String[] words = {"app", "apple"};
+    assertTrue(allienDictionary.isAlienSorted(words, order));
+  }
+
+  @Test
+  public void testCase3() {
+    String[] words = {"app", "", "apple"};
+    assertFalse(allienDictionary.isAlienSorted(words, order));
+  }
+
 }
